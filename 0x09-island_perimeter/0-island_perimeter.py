cf44746e36317c9b47e5fe3ea84008aca8d412a5
Eliud254 <eliudobure@gmail.com>
2024-07-07 06:16:23 -0700

    fix

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,57 +1,8 @@
 #!/usr/bin/python3
 """
-Defines the function island_perimeter
-to compute the perimeter
-of an island represented in a grid.
+Defines the function island_perimeter to compute the perimeter
+of an island described in a grid.
 """
-
-bound_4 = set()
-bound_3 = set()
-bound_2 = set()
-bound_1 = set()
-
-
-def boundary(grid, i, j):
-    """
-    Determine the number of exposed boundaries
-    for a cell and categorize it.
-
-    Args:
-        grid (list): 2D list representing the island and water.
-        i (int): Row index of the cell.
-        j (int): Column index of the cell.
-    """
-    boundaries = 0
-    try:
-        if i == 0 or grid[i-1][j] == 0:
-            boundaries += 1
-    except IndexError:
-        boundaries += 1
-    try:
-        if grid[i+1][j] == 0:
-            boundaries += 1
-    except IndexError:
-        boundaries += 1
-    try:
-        if grid[i][j+1] == 0:
-            boundaries += 1
-    except IndexError:
-        boundaries += 1
-    try:
-        if j == 0 or grid[i][j-1] == 0:
-            boundaries += 1
-    except IndexError:
-        boundaries += 1
-
-    if boundaries == 1:
-        bound_1.add((i, j))
-    elif boundaries == 2:
-        bound_2.add((i, j))
-    elif boundaries == 3:
-        bound_3.add((i, j))
-    elif boundaries == 4:
-        bound_4.add((i, j))
-
 
 def island_perimeter(grid):
     """
@@ -63,18 +14,24 @@ def island_perimeter(grid):
     Returns:
         int: The perimeter of the island.
     """
-    if not grid:
-        return 0
-
+    perimeter = 0
     rows = len(grid)
     cols = len(grid[0])
 
     for i in range(rows):
         for j in range(cols):
             if grid[i][j] == 1:
-                boundary(grid, i, j)
-                if bound_4:
-                    return 4
+                # Check top boundary
+                if i == 0 or grid[i-1][j] == 0:
+                    perimeter += 1
+                # Check bottom boundary
+                if i == rows - 1 or grid[i+1][j] == 0:
+                    perimeter += 1
+                # Check left boundary
+                if j == 0 or grid[i][j-1] == 0:
+                    perimeter += 1
+                # Check right boundary
+                if j == cols - 1 or grid[i][j+1] == 0:
+                    perimeter += 1
 
-    perimeter = (len(bound_3) * 3) + (len(bound_2) * 2) + len(bound_1)
     return perimeter
