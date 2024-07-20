727411035acf64c1040dcfd993c2738981ee3228
Eliud254 <eliudobure@gmail.com>
2024-07-20 10:27:16 -0700

    Make change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,44 +1,24 @@
 #!/usr/bin/python3
-"""Making Change Problem 0-making_change.py
-"""
+
+""" makeChange function"""
 
 
 def makeChange(coins, total):
     """
-    Calculate the minimum number of coins
-    needed to make a given amount of change.
-
-    Args:
-        coins (list): List of available coin denominations.
-        total (int): The total amount for which change is needed.
-
-    Returns:
-        int: Minimum number of coins required to make the change.
-        -1: If it is not possible to make the change with the given coins.
+    Returns: few numbers of coins needed to meet total
+        If total is 0 or less, return 0
+        If total cannot be met by any number of coins you have, return -1
     """
-
-    # If the total amount is zero or negative, return -1
+    if not coins or coins is None:
+        return -1
     if total <= 0:
-        return -1
-
-    # Sort the coins in descending order to
-    sorted_coins = sorted(coins, reverse=True)
-    count = 0
-    remaining_value = total
-    index = 0
-
-    # Iterate through the sorted coin list
-    while remaining_value > 0 and index < len(sorted_coins):
-        # If the current coin can be used to make change
-        if remaining_value >= sorted_coins[index]:
-            remaining_value -= sorted_coins[index]
-            count += 1
-        else:
-            # Move to the next smaller coin
-            index += 1
-
-    # If change cannot be made with the given coins, return -1
-    if remaining_value != 0:
-        return -1
-
-    return count
+        return 0
+    change = 0
+    coins = sorted(coins)[::-1]
+    for coin in coins:
+        while coin <= total:
+            total -= coin
+            change += 1
+        if (total == 0):
+            return change
+    return -1
